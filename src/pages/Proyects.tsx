72f62c3d2b8dcc658aa126bfbbd3aff5dab53311
Soy-Mauricio <maurosana6@gmail.com>
2025-02-27 18:38:51 -0500

    Creación de vista Proyectos

--- src/pages/Proyects.tsx
+++ src/pages/Proyects.tsx
@@ -44,7 +44,7 @@ const Proyects: React.FC = () => {
 
         <div id="todos">
           <Info
-              title="PROYECTOS"
+              title="PROYECTO"
               content={[
               "Asumo la creación de espacios social, económica y ambientalmente equilibrados como un objetivo esencial en mi trabajo como arquitecta. Considero que la arquitectura no debe limitarse únicamente a la expresión artística o a la eficiencia técnica, sino que debe ser el punto de encuentro entre ambos enfoques. Tradicionalmente, la disciplina se ha visto dominada por dos extremos: uno altamente expresivo y vanguardista, pero muchas veces costoso e irrealista, y otro estrictamente funcional y racional, que aunque práctico, puede carecer de ambición e inspiración.",
               "Cada proyecto es una oportunidad para innovar y encontrar soluciones que no solo respondan a las necesidades del presente, sino que también sean sostenibles a largo plazo. Me motiva diseñar espacios que inspiren, que reflejen identidad y que mejoren la calidad de vida de las personas. Para mí, la arquitectura no es solo una disciplina técnica, sino una herramienta de transformación que puede dar forma a ciudades más humanas, eficientes y armónicas con su contexto.",
